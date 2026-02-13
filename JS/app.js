2c58a0e239b8ecf279b599ac59138df0792c2c3b
Emelinur <emelinuryzgc@gmail.com>
2026-02-13 17:58:19 +0300

    feat(theme): enhance dark theme styling and update theme toggle icons

--- JS/app.js
+++ JS/app.js
@@ -1,22 +1,26 @@
 // import { getData } from "./helpers";
 const themaSelect = document.querySelector("#theme-toggle");
+const sunIcon = document.querySelector("#sun-icon");
+const moonIcon = document.querySelector("#moon-icon");
 
 themaSelect.addEventListener("change",  (e) => {
 const selectedThema = e.target.checked
 if(selectedThema){
 
 document.body.classList.add("dark");
+sunIcon.classList.remove("thema-sun-icon");
+sunIcon.classList.add("thema-sun-icon-dark");
+moonIcon.classList.remove("thema-moon-icon");
+moonIcon.classList.add("thema-moon-icon-dark");
+
 
 } else {
-
 document.body.classList.remove("dark");
-
+sunIcon.classList.remove("thema-sun-icon-dark");
+sunIcon.classList.add("thema-sun-icon");
+moonIcon.classList.remove("thema-moon-icon-dark");
+moonIcon.classList.add("thema-moon-icon");
 }
+localStorage.setItem("quiz-theme", e.target.checked ? "dark" : "light");
 });
-
-
-
-  // const selectedThema = e.target.checked ? "dark" : "light";
-  // selectedThema === "dark" ? document.documentElement.setAttribute("data-theme", "dark") : document.documentElement.removeAttribute("data-theme");
-  // console.log("Selected thema:", selectedThema); 
 

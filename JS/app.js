5db24e5e32283f612f5ac5c969f05486e942a6ce
Emelinur <emelinuryzgc@gmail.com>
2026-02-15 04:02:38 +0300

    feat: enhance theme toggle functionality and improve layout for better accessibility and readability

--- JS/app.js
+++ JS/app.js
@@ -3,20 +3,9 @@ const themaSelect = document.querySelector("#theme-toggle");
 
 themaSelect.addEventListener("change",  (e) => {
 const selectedThema = e.target.checked
-if(selectedThema){
 
-document.body.classList.add("dark");
-
-} else {
-
-document.body.classList.remove("dark");
-
-}
+selectedThema ? document.body.classList.add("dark") : document.body.classList.remove("dark");
+localStorage.setItem("selectedThema", selectedThema);
 });
 
-
-
-  // const selectedThema = e.target.checked ? "dark" : "light";
-  // selectedThema === "dark" ? document.documentElement.setAttribute("data-theme", "dark") : document.documentElement.removeAttribute("data-theme");
-  // console.log("Selected thema:", selectedThema); 
 

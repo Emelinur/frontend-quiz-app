5ca104ce56718f5f07f23b88bd10240c8f382f42
Emelinur <emelinuryzgc@gmail.com>
2026-02-16 17:51:34 +0300

    feat(theme): refactor theme toggle functionality and improve styling for better accessibility

--- JS/app.js
+++ JS/app.js
@@ -1,26 +1,10 @@
 // import { getData } from "./helpers";
 const themaSelect = document.querySelector("#theme-toggle");
-const sunIcon = document.querySelector("#sun-icon");
-const moonIcon = document.querySelector("#moon-icon");
 
 themaSelect.addEventListener("change",  (e) => {
 const selectedThema = e.target.checked
-if(selectedThema){
-
-document.body.classList.add("dark");
-sunIcon.classList.remove("thema-sun-icon");
-sunIcon.classList.add("thema-sun-icon-dark");
-moonIcon.classList.remove("thema-moon-icon");
-moonIcon.classList.add("thema-moon-icon-dark");
-
-
-} else {
-document.body.classList.remove("dark");
-sunIcon.classList.remove("thema-sun-icon-dark");
-sunIcon.classList.add("thema-sun-icon");
-moonIcon.classList.remove("thema-moon-icon-dark");
-moonIcon.classList.add("thema-moon-icon");
-}
-localStorage.setItem("quiz-theme", e.target.checked ? "dark" : "light");
+selectedThema ? document.body.classList.add("dark") : document.body.classList.remove("dark");
+localStorage.setItem("selectedThema", selectedThema);
 });
+
 
